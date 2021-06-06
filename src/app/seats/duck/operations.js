c1e798a7fc3be5b9e42f5b19d1086abc533d629e
dkotula <dkotula.wieliczka@gmail.com>
2021-06-06 21:13:23 +0200

    Added default seats with checked textbox

--- src/app/seats/duck/operations.js
+++ src/app/seats/duck/operations.js
@@ -1,14 +1,60 @@
 import actions from './actions'
 
-const getDefaultList = (seats, numberOfSeats, nextTo,dispatch) => {
-    for(const seat of seats) {
-        if (!seat.reserved) {
-            dispatch(actions.addDefault(seat))
-            if (--numberOfSeats <= 0) {
-                break;
+const getDefaultList = (seats, numberOfSeats, nextTo, dispatch) => {
+    if (nextTo) {
+        const list = findSeats(seats, numberOfSeats)
+        list.forEach(seat => dispatch(actions.addDefault(seat)))
+    }
+    else {
+        for(const seat of seats) {
+            if (!seat.reserved) {
+                dispatch(actions.addDefault(seat))
+                if (--numberOfSeats <= 0) {
+                    break;
+                }
             }
         }
     }
+}
+
+const findSeats = (seats, numberOfSeats) => {
+    let list = []
+    let nearNumber = 0;
+    for (const seat of seats) {
+        if (seat.reserved) {
+            nearNumber = 0
+            list = []
+            continue
+        }
+        if (nearNumber === 0) {
+            nearNumber = 1
+            list = [seat]
+            continue
+        }
+
+        if (seat.cords.x === list[list.length - 1].cords.x) {
+            if (seat.cords.y === list[list.length - 1].cords.y + 1) {
+                nearNumber++
+                list.push(seat)
+            }
+            else {
+                nearNumber = 1
+                list = [seat]
+            }
+        }
+        else {
+            nearNumber = 1
+            list = [seat]
+        }
+
+        if (list.length === numberOfSeats) {
+            break
+        }
+    }
+    if (list.length !== numberOfSeats) {
+        list = []
+    }
+    return list
 }
 
 const fetchSeats = async () => {
